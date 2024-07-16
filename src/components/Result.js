680105ad9d2773d057380c217d1ef289a785e29c
eun <nue031@kunsan.co.kr>
2024-07-17 03:59:45 +0900

    result commit

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,45 +1,26 @@
-
-/*import React from 'react';
-import '../styles/Result.css'; 
-
+import React from "react";
+import "../styles/Result.css";
 
 const Result = () => {
   return (
     <div className="about-container">
-      <header className="header">
-        
-      </header>
       <main className="about-main">
         <section className="about-text">
           <h1 className="about-title1">Type </h1>
           <h1 className="about-title2">Classic</h1>
-          <p>You might talk about your origin story, your team, highlight awards or recognitions, or share photos. Tap into your creativity. You’ve got this. The way you tell your story online can make all the difference.</p>
-          <p>Don’t worry about sounding professional. Sound like you. There are over 1.5 million websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.</p>
-        </section>
-        <section className="about-image">
-          <img src="/images/classic1.jpg" alt="About Us" />
-        </section>
-      </main>
-    </div>
-  );
-};
-
-export default Result;*/
-
-import React from 'react';
-import '../styles/Result.css'; 
-
-
-const Result = () => {
-  return (
-    <div className="about-container">
-      
-      <main className="about-main">
-        <section className="about-text">
-          <h1 className="about-title1">Type </h1>
-          <h1 className="about-title2">Classic</h1>
-          <p>You might talk about your origin story, your team, highlight awards or recognitions, or share photos. Tap into your creativity. You’ve got this. The way you tell your story online can make all the difference.</p>
-          <p>Don’t worry about sounding professional. Sound like you. There are over 1.5 million websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.</p>
+          <p>
+            You might talk about your origin story, your team, highlight awards
+            or recognitions, or share photos. Tap into your creativity. You’ve
+            got this. The way you tell your story online can make all the
+            difference.
+          </p>
+          <p>
+            Don’t worry about sounding professional. Sound like you. There are
+            over 1.5 million websites out there, but your story is what’s going
+            to separate this one from the rest. If you read the words back and
+            don’t hear your own voice in your head, that’s a good sign you still
+            have more work to do.
+          </p>
         </section>
         <section className="about-image">
           <img src="/images/classic1.jpg" alt="About Us" />
